b11cda18dccd64d445ff7a9dc4825951c72ffac6
TrueMolni <truemolni@gmail.com>
2022-12-25 16:36:05 +0200

    1

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './js/mobile-menu';
+import './js/modal';
+import './js/scrollup';
+import './js/slide';
+import './js/prod';
+import './js/modal-our-locations';
+import './js/modal-read-more';
---
+++ src/js/modal-our-locations.js
@@ -0,0 +1,15 @@
+const refs = {
+  openModalBtn: document.querySelector('[data-open-modal-locations]'),
+  closeModalBtn: document.querySelector('[data-close-modal-locations]'),
+  modalwindow: document.querySelector('[data-modal-our-locations]'),
+};
+console.log(refs.openModalBtn);
+// const openModalBtn = document.querySelectorAll('[data-modal-window-open]');
+// openModalBtn.forEach(element => element.addEventListener('click', toggleModal));
+
+refs.openModalBtn.addEventListener('click', toggleModal);
+refs.closeModalBtn.addEventListener('click', toggleModal);
+
+function toggleModal() {
+  refs.modalwindow.classList.toggle('is-hidden');
+}
